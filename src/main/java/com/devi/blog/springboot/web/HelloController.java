642e5c0bca58795aa699c6dbb9f1e236e1aa60d1
HappyLuluMain <happylulumain@gmail.com>
2022-04-20 20:16:46 +0900

    Add Posts

--- src/main/java/com/devi/blog/springboot/web/HelloController.java
+++ src/main/java/com/devi/blog/springboot/web/HelloController.java
@@ -1,13 +1,17 @@
 package com.devi.blog.springboot.web;
 
+import com.devi.blog.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String home(){
-        return "hello";
+    public HelloResponseDto home(@RequestParam("name") String name,
+                                 @RequestParam("amount") int amount){
+
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/devi/blog/springboot/web/HelloControllerTest.java
+++ src/test/java/com/devi/blog/springboot/web/HelloControllerTest.java
@@ -7,8 +7,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -18,11 +18,25 @@ public class HelloControllerTest {
     private MockMvc mvc;
 
     @Test
-    public void hello_return() throws Exception{
+    public void helloReturn() throws Exception{
         String hello = "hello";
 
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDtoReturn() throws Exception{
+        String name = "test";
+        int amount = 1000;
+
+        mvc.perform(get("/hello")
+                .param("name", name)
+                //String.valueOf를 이용해서 amount를 String으로 변경해 주입
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
